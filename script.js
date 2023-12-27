ae4f7c00f4171d8f6f08c03be0209490b69b3507
Neel Mishra <neelmishra9125@gmail.com>
2023-12-27 09:41:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,63 @@
-let startTime;
-let interval;
-let running = false;
-let lapCount = 1;
+class Stopwatch {
+    constructor() {
+        this.startTime = 0;
+        this.interval = 0;
+        this.running = false;
+        this.lapCount = 1;
+        this.startStopButton = document.getElementById("startStop");
+        this.resetButton = document.getElementById("reset");
+        this.lapButton = document.getElementById("lap");
+        this.display = document.getElementById("display");
+        this.laps = document.getElementById("laps");
 
-function startStop() {
-    if (running) {
-        clearInterval(interval);
-        document.getElementById("startStop").innerHTML = "Start";
-    } else {
-        startTime = Date.now() - (lapCount === 1 ? 0 : lapCount - 1) * interval;
-        interval = setInterval(updateTime, 10);
-        document.getElementById("startStop").innerHTML = "Stop";
+        this.startStopButton.addEventListener('click', () => this.startStop());
+        this.resetButton.addEventListener('click', () => this.reset());
+        this.lapButton.addEventListener('click', () => this.lap());
     }
-    running = !running;
-}
 
-function reset() {
-    clearInterval(interval);
-    document.getElementById("startStop").innerHTML = "Start";
-    document.getElementById("display").innerHTML = "00:00:00";
-    document.getElementById("laps").innerHTML = "";
-    lapCount = 1;
-    running = false;
-}
+    startStop() {
+        if (this.running) {
+            clearInterval(this.interval);
+            this.startStopButton.innerHTML = "Start";
+        } else {
+            this.startTime = Date.now() - (this.lapCount === 1 ? 0 : this.lapCount - 1) * this.interval;
+            this.interval = setInterval(() => this.updateTime(), 10);
+            this.startStopButton.innerHTML = "Stop";
+        }
+        this.running = !this.running;
+    }
 
-function lap() {
-    if (running) {
-        const lapTime = formatTime(Date.now() - startTime);
-        const lapList = document.getElementById("laps");
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `Lap ${lapCount}: ${lapTime}`;
-        lapList.appendChild(listItem);
-        lapCount++;
+    reset() {
+        clearInterval(this.interval);
+        this.startStopButton.innerHTML = "Start";
+        this.display.innerHTML = "00:00:00";
+        this.laps.innerHTML = "";
+        this.lapCount = 1;
+        this.running = false;
+    }
+
+    lap() {
+        if (this.running) {
+            const lapTime = this.formatTime(Date.now() - this.startTime);
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `Lap ${this.lapCount}: ${lapTime}`;
+            this.laps.appendChild(listItem);
+            this.lapCount++;
+        }
+    }
+
+    updateTime() {
+        const currentTime = Date.now() - this.startTime;
+        this.display.innerHTML = this.formatTime(currentTime);
+    }
+
+    formatTime(time) {
+        const minutes = Math.floor((time / 60000) % 60);
+        const seconds = Math.floor((time / 1000) % 60);
+        const milliseconds = (time % 1000).toString().slice(0, 2);
+        return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}.${milliseconds}`;
     }
 }
 
-function updateTime() {
-    const currentTime = Date.now() - startTime;
-    document.getElementById("display").innerHTML = formatTime(currentTime);
-}
-
-function formatTime(time) {
-    const minutes = Math.floor((time / 60000) % 60);
-    const seconds = Math.floor((time / 1000) % 60);
-    const milliseconds = (time % 1000).toString().slice(0, 2);
-    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}.${milliseconds}`;
-}
-
-reset();
+const stopwatch = new Stopwatch();
+stopwatch.reset();
